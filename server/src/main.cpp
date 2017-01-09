973a8c61de5e675d177426cba6a164a48631d52e
Kioshi <Kioshi@a.b>
2017-01-10 00:10:01 +0100

    wtf

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -14,7 +14,9 @@ int main(int argc, char** argv)
     {
         std::cout << "Zadejte port: ";
         std::cin >> port;
+        std::cout << port << std::endl;
     } while (port <= 0 && port >= 65536);
+    std::cout << port << std::endl;
     std::srand((unsigned int)std::time(0));
 #else
     port = 10001;
