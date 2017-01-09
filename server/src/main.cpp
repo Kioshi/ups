5d563cd240c4f28afcf249193fb639158ef19a99
Kioshi <Kioshi@a.b>
2017-01-10 00:11:09 +0100

    Agrh!

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -14,9 +14,7 @@ int main(int argc, char** argv)
     {
         std::cout << "Zadejte port: ";
         std::cin >> port;
-        std::cout << port << std::endl;
-    } while (port <= 0 && port >= 65536);
-    std::cout << port << std::endl;
+    } while (port <= 0 || port >= 65536);
     std::srand((unsigned int)std::time(0));
 #else
     port = 10001;
